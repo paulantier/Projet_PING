4453ddc606ed79284bde1567c01412f31a2d623b
paulantier <105341347+paulantier@users.noreply.github.com>
2022-06-22 17:16:01 +0200

    Add files via upload

---
+++ raspberrypi_TEMPSREEL/LeptonSDKEmb32PUB/CMakeFiles/LeptonSDK.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/pi/LeptonModule/software/LeptonSDKEmb32PUB/CMakeFiles/3.16.3/CompilerIdC/CMakeCCompilerId.c" "/home/pi/LeptonModule/software/LeptonSDKEmb32PUB/CMakeFiles/LeptonSDK.dir/CMakeFiles/3.16.3/CompilerIdC/CMakeCCompilerId.c.o"
+  "/home/pi/LeptonModule/software/LeptonSDKEmb32PUB/LEPTON_AGC.c" "/home/pi/LeptonModule/software/LeptonSDKEmb32PUB/CMakeFiles/LeptonSDK.dir/LEPTON_AGC.c.o"
+  "/home/pi/LeptonModule/software/LeptonSDKEmb32PUB/LEPTON_I2C_Protocol.c" "/home/pi/LeptonModule/software/LeptonSDKEmb32PUB/CMakeFiles/LeptonSDK.dir/LEPTON_I2C_Protocol.c.o"
+  "/home/pi/LeptonModule/software/LeptonSDKEmb32PUB/LEPTON_I2C_Service.c" "/home/pi/LeptonModule/software/LeptonSDKEmb32PUB/CMakeFiles/LeptonSDK.dir/LEPTON_I2C_Service.c.o"
+  "/home/pi/LeptonModule/software/LeptonSDKEmb32PUB/LEPTON_OEM.c" "/home/pi/LeptonModule/software/LeptonSDKEmb32PUB/CMakeFiles/LeptonSDK.dir/LEPTON_OEM.c.o"
+  "/home/pi/LeptonModule/software/LeptonSDKEmb32PUB/LEPTON_SDK.c" "/home/pi/LeptonModule/software/LeptonSDKEmb32PUB/CMakeFiles/LeptonSDK.dir/LEPTON_SDK.c.o"
+  "/home/pi/LeptonModule/software/LeptonSDKEmb32PUB/LEPTON_SYS.c" "/home/pi/LeptonModule/software/LeptonSDKEmb32PUB/CMakeFiles/LeptonSDK.dir/LEPTON_SYS.c.o"
+  "/home/pi/LeptonModule/software/LeptonSDKEmb32PUB/LEPTON_VID.c" "/home/pi/LeptonModule/software/LeptonSDKEmb32PUB/CMakeFiles/LeptonSDK.dir/LEPTON_VID.c.o"
+  "/home/pi/LeptonModule/software/LeptonSDKEmb32PUB/crc16fast.c" "/home/pi/LeptonModule/software/LeptonSDKEmb32PUB/CMakeFiles/LeptonSDK.dir/crc16fast.c.o"
+  "/home/pi/LeptonModule/software/LeptonSDKEmb32PUB/raspi_I2C.c" "/home/pi/LeptonModule/software/LeptonSDKEmb32PUB/CMakeFiles/LeptonSDK.dir/raspi_I2C.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ raspberrypi_TEMPSREEL/LeptonSDKEmb32PUB/CMakeFiles/LeptonSDK.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LeptonSDK.dir/CMakeFiles/3.16.3/CompilerIdC/CMakeCCompilerId.c.o"
+  "CMakeFiles/LeptonSDK.dir/LEPTON_AGC.c.o"
+  "CMakeFiles/LeptonSDK.dir/LEPTON_I2C_Protocol.c.o"
+  "CMakeFiles/LeptonSDK.dir/LEPTON_I2C_Service.c.o"
+  "CMakeFiles/LeptonSDK.dir/LEPTON_OEM.c.o"
+  "CMakeFiles/LeptonSDK.dir/LEPTON_SDK.c.o"
+  "CMakeFiles/LeptonSDK.dir/LEPTON_SYS.c.o"
+  "CMakeFiles/LeptonSDK.dir/LEPTON_VID.c.o"
+  "CMakeFiles/LeptonSDK.dir/crc16fast.c.o"
+  "CMakeFiles/LeptonSDK.dir/raspi_I2C.c.o"
+  "libLeptonSDK.a"
+  "libLeptonSDK.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/LeptonSDK.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
